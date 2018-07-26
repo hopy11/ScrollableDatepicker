746edad0b7036d022c0bdc7e27031d109cb3f7d7
hopy <hopy@livec.cn>
2018-07-26 15:48:52 +0800

    开始增加Month类型支持

--- Sources/DayCell.swift
+++ Sources/DayCell.swift
@@ -12,6 +12,11 @@ open class DayCell: UICollectionViewCell {
     @IBOutlet public weak var weekDayLabel: UILabel!
     @IBOutlet public weak var monthLabel: UILabel!
     @IBOutlet public weak var selectorView: UIView!
+    
+    enum CellType {
+        case day
+        case month
+    }
 
     static var ClassName: String {
         return String(describing: self)
@@ -20,26 +25,32 @@ open class DayCell: UICollectionViewCell {
 
     // MARK: - Setup
 
-    func setup(date: Date, style: DayStyleConfiguration) {
+    func setup(date: Date, style: DayStyleConfiguration, type:CellType = .day) {
         let formatter = DateFormatter()
 
-        formatter.dateFormat = "dd"
-        dateLabel.text = formatter.string(from: date)
-        dateLabel.font = style.dateTextFont ?? dateLabel.font
-        dateLabel.textColor = style.dateTextColor ?? dateLabel.textColor
-
-        formatter.dateFormat = "EEE"
-        weekDayLabel.text = formatter.string(from: date).uppercased()
-        weekDayLabel.font = style.weekDayTextFont ?? weekDayLabel.font
-        weekDayLabel.textColor = style.weekDayTextColor ?? weekDayLabel.textColor
-
-        formatter.dateFormat = "MMMM"
-        monthLabel.text = formatter.string(from: date).uppercased()
-        monthLabel.font = style.monthTextFont ?? monthLabel.font
-        monthLabel.textColor = style.monthTextColor ?? monthLabel.textColor
-
-        selectorView.backgroundColor = style.selectorColor ?? UIColor.clear
-        backgroundColor = style.backgroundColor ?? backgroundColor
+        if type == .day{
+            formatter.dateFormat = "dd"
+            dateLabel.text = formatter.string(from: date)
+            dateLabel.font = style.dateTextFont ?? dateLabel.font
+            dateLabel.textColor = style.dateTextColor ?? dateLabel.textColor
+            
+            formatter.dateFormat = "EEE"
+            weekDayLabel.text = formatter.string(from: date).uppercased()
+            weekDayLabel.font = style.weekDayTextFont ?? weekDayLabel.font
+            weekDayLabel.textColor = style.weekDayTextColor ?? weekDayLabel.textColor
+            
+            formatter.dateFormat = "MMMM"
+            monthLabel.text = formatter.string(from: date).uppercased()
+            monthLabel.font = style.monthTextFont ?? monthLabel.font
+            monthLabel.textColor = style.monthTextColor ?? monthLabel.textColor
+            
+            selectorView.backgroundColor = style.selectorColor ?? UIColor.clear
+            backgroundColor = style.backgroundColor ?? backgroundColor
+        }else if type == .month{
+            
+        }
+        
+        
     }
 
 }
